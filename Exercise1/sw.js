c830ecd7b26796e54d049943e44caf59225e5387
WebTorill <46745387+WebTorill@users.noreply.github.com>
2019-01-28 14:31:06 +0100

    Add files via upload

---
+++ Exercise1/sw.js
@@ -0,0 +1,24 @@
+// Chrome's currently missing some useful cache methods,
+// this polyfill adds them.
+importScripts('serviceworker-cache-polyfill.js');
+
+// Here comes the install event!
+// This only happens once, when the browser sees this
+// version of the ServiceWorker for the first time.
+self.addEventListener('install', function(event) {
+  // We pass a promise to event.waitUntil to signal how 
+  // long install takes, and if it failed
+  event.waitUntil(
+    // We open a cache…
+    caches.open('simple-sw-v1').then(function(cache) {
+      // And add resources to it
+      return cache.addAll([
+        //TODO: add the files that needs to be cached
+      ]);
+    })
+  );
+});
+
+//TODO: create the EventListener for 'fetch'.
+//it should check if the url exists in the cache, if so get the response from the cache, otherwise get it
+
